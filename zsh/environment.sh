b4c2d2c5d14e2193e0814984c0cbab2f05ac1edf
Connor Littleton <littletonconnor@gmail.com>
2024-07-14 19:27:14 -0700

    remove java stuff

--- zsh/environment.sh
+++ zsh/environment.sh
@@ -1,7 +1,3 @@
 export ZSH_CONFIG="$HOME/.config/dot-files/zsh"
 export DOT_FILES="$HOME/.config/dot-files"
 export SFE_HOME="$HOME/work/frontend/santafe"
-
-export JAVA_HOME=$(/usr/libexec/java_home)
-export PATH="$JAVA_HOME/bin:$PATH"
-export SCALA_HOME=$(brew --prefix)/opt/scala/idea
